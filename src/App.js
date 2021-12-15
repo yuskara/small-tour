52077acab0bc254d61513b43dd0aaf8b213f2ee0
yuskara <ykaraca@gmail.com>
2021-12-15 06:24:03 -0500

    helping tour

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TourProvider } from '@reactour/tour'
+
+import { Placeholder,Paragraphs } from './components/utils';
+import { TextPlaceholder } from './components/utils';
+import {
+  doSteps,
+  BeachIcon,
+  BoatIcon,
+  BallIcon,
+  GuideIcon,
+  IcecreamIcon,steps,
+} from './components/utils'
+const demoId = 'withTour'
+const iconsteps = doSteps(demoId)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+
+        <TourProvider steps={iconsteps}>
+  <Placeholder demoId={demoId} />
+</TourProvider>
+       
+
+        
+      </header>
+      <TourProvider steps={steps}>
+    <Paragraphs />
+  </TourProvider>
+
+  <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
